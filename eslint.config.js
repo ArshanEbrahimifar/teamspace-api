516a1b725ebc41ea4cd1ce0f02ba6aaa16eb4701
Arshan Ebrahimifar <227443762+ArshanEbrahimifar@users.noreply.github.com>
2026-07-15 19:38:53 +0300

    chore: configure ESlint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist", "node_modules", "coverage"],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["src/**/*.ts"],
+
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+        },
+      ],
+
+      "no-console": "off",
+    },
+  },
+
+  eslintConfigPrettier,
+);
