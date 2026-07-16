49bd8086348c2900c813f9797b9a335e810f5ee9
Arshan Ebrahimifar <227443762+ArshanEbrahimifar@users.noreply.github.com>
2026-07-16 22:01:18 +0300

    feat: configure Prisma with PostgreSQL

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
     .enum(["development", "test", "production"])
     .default("development"),
   PORT: z.coerce.number().int().positive().default(5000),
+  DATABASE_URL: z.string().url(),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
